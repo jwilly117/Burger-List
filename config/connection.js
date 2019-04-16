ee03e0461ba2fc5e4e5449a4f6b51657453ef4a7
Jake Williams <jakewilliams117@gmail.com>
2019-04-16 12:04:32 -0400

    Fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql');
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Pringles117',
-    database: 'burgers_db'
-})
+
+let connection
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Pringles117',
+        database: 'burgers_db'
+    });
+};
+
+
 
 connection.connect();
 
